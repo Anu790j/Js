b96099d4354d21e1469386f4ab78402b43da975c
Anubhav Jain <91656728+Anu790j@users.noreply.github.com>
2024-03-12 16:16:35 +0000

    variables

---
+++ js_Basics_01/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 1222
+let accountEmail = "anubhav@yahoo.com"
+var accPass = "1234"
+accCity="Roorkee"
+
+accountEmail="anu@yahoo.com"
+accPass="4321"
+accCity="Bengalur"
+
+console.table([accountEmail,accPass,accCity])
